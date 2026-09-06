e52478f331095a3d62f6776ccf577c9790779c26
palakgit001 <palak00981@gmail.com>
2026-09-06 16:11:14 +0530

    Add Processing component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Form from './components/Form';
+import Processing from './components/Processing';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1>ONI - GRADE</h1>
       <h2>Onion Grading : New Batch</h2>
       <Form />
+      <Processing />
     </div>
   );
 }
