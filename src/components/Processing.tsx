1771b826bd26fbe65eb0e16ac7e3df139c36450f
palakgit001 <palak00981@gmail.com>
2026-09-06 16:03:41 +0530

    Add Processing component with loading state

--- src/components/Processing.tsx
+++ src/components/Processing.tsx
@@ -1,1 +1,14 @@
+import { useState } from "react";
 
+function Processing() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [result, setResult] = useState("");
+
+  return (
+    <div>
+      Processing component works
+    </div>
+  );
+}
+
+export default Processing;
