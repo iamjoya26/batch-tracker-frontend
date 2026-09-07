9362d4bc740eb1befaed735d553c5ed689403eef
Palak <palak00981@gmail.com>
2026-09-07 21:57:23 +0530

    Add stage variable in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import ImageUpload from './components/ImageUpload';
 import Processing from './components/Processing';
 
 function App() {
+  const [stage, setStage] = useState('form');
   return (
     <div className="form-card">
       <h1>ONI - GRADE</h1>
